3a89adacc9ad8cafbac5b699c68631e1e3f3143d
ongcu <dlgycns0813@naver.com>
2022-04-22 02:54:10 +0900

    메인 화면 추가개발

---
+++ Assets/01 Scripts/UI/MainScreen.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.EventSystems;
+
+public class MainScreen : MonoBehaviour, IPointerDownHandler
+{
+    public TextMeshProUGUI Text;
+    public CanvasGroup mainGroup;
+    public Image image;
+
+
+    private void Start()
+    {
+        StartCoroutine("Logoshow");
+    }
+
+
+    IEnumerator Logoshow()
+    {
+        while(image.color.a < 1.0f)
+        {
+            image.color = new Color(1.0f, 1.0f, 1.0f, image.color.a + Time.deltaTime / 2.5f);
+            yield return null;
+        }
+        Text.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+        StartCoroutine("textOutshow");
+    }
+    IEnumerator textInshow()
+    {
+
+        while (Text.color.a < 1.0f)
+        {
+            Text.color = new Color(1.0f, 1.0f, 1.0f, Text.color.a + Time.deltaTime /2.0f);
+            yield return null;
+        }
+        StartCoroutine("textOutshow");
+        
+    }
+    IEnumerator textOutshow()
+    {
+
+        while (Text.color.a > 0f)
+        {
+            Text.color = new Color(1.0f, 1.0f, 1.0f, Text.color.a - Time.deltaTime / 2.0f);
+            yield return null;
+        }
+        StartCoroutine("textInshow");
+
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (Text.gameObject.activeSelf == true) {
+            image.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+            StopCoroutine("Logoshow");
+            StopCoroutine("textInshow");
+            StopCoroutine("textOutshow");
+            Text.gameObject.SetActive(false);
+            mainGroup.alpha = 1;
+            mainGroup.interactable = true;
+            mainGroup.blocksRaycasts = true;
+        }
+    }
+}
